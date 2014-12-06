28a8a6643c4b32ab968eb5c803b9e650e8cbcdce
tolmachevroman <tolmachevroman@gmail.com>
2014-12-06 17:10:27 -0300

    Show list of last calls on the map

--- DigevoSample/app/src/main/java/app/sample/digevo/activities/PlacesActivity.java
+++ DigevoSample/app/src/main/java/app/sample/digevo/activities/PlacesActivity.java
@@ -6,24 +6,27 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import app.sample.digevo.R;
 import app.sample.digevo.network.ClientApi;
+import app.sample.digevo.network.entities.Call;
 import app.sample.digevo.network.responses.LastCallsResponse;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class PlacesActivity extends FragmentActivity {
+public class PlacesActivity extends FragmentActivity implements GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMarkerClickListener {
 
-    public static final int ZOOM = 15;
+    public static final int ZOOM = 12;
     public static final int BEARING = 90;
     public static final int TILT = 40;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
@@ -47,11 +50,11 @@ public class PlacesActivity extends FragmentActivity {
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
      * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
+     * <p/>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
      * install/update the Google Play services APK on their device.
-     * <p>
+     * <p/>
      * A user can return to this FragmentActivity after following the prompt and correctly
      * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
      * have been completely destroyed during this process (it is likely that it would only be
@@ -74,7 +77,7 @@ public class PlacesActivity extends FragmentActivity {
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
-     * <p>
+     * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
@@ -82,13 +85,15 @@ public class PlacesActivity extends FragmentActivity {
         //enable show my location
         mMap.setMyLocationEnabled(true);
 
+        //respond on info window touch
+        mMap.setOnInfoWindowClickListener(this);
+
         //zoom a bit to my position
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
 
         Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-        if (location != null)
-        {
+        if (location != null) {
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                     new LatLng(location.getLatitude(), location.getLongitude()), ZOOM));
 
@@ -102,7 +107,6 @@ public class PlacesActivity extends FragmentActivity {
 
         }
 
-//        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
     private void getLastCalls() {
@@ -110,8 +114,21 @@ public class PlacesActivity extends FragmentActivity {
         mClientApi.getUntakenLessons(new Callback<LastCallsResponse>() {
             @Override
             public void success(LastCallsResponse lastCallsResponse, Response response) {
-                if(lastCallsResponse != null) {
-                    Log.d("CALLS SIZE", lastCallsResponse.getData().getCalls().size() + "");
+                if (lastCallsResponse != null) {
+
+                    //show all points on the map
+                    for (Call call : lastCallsResponse.getData().getCalls()) {
+
+                        mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(call.getLatitude(), call.getLongitude()))
+                                .title(call.getTitle())
+                                .snippet(call.getContent() + " " + call.getTimestamp().toString()));
+                    }
+
+                    //zoom the map to fit all points
+//                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+//                            new LatLng(41.889, -87.622), 16));
+
                 }
             }
 
@@ -120,5 +137,18 @@ public class PlacesActivity extends FragmentActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        //center on the marker
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(marker.getPosition().latitude, marker.getPosition().longitude), 16));
+        return true;
     }
 }
